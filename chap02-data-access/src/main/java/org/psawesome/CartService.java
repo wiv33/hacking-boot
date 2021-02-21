e68fb9c783df624c8df2a2266962f35e1538d6da
PSK <40161102+wiv33@users.noreply.github.com>
2021-02-22 00:12:09 +0900

    chap 02: move service

--- chap02-data-access/src/main/java/org/psawesome/CartService.java
+++ chap02-data-access/src/main/java/org/psawesome/CartService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 /**
  * package: org.psawesome
  * author: PS
@@ -13,11 +15,25 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class CartService {
 
-  private final PsItemRepository itemRepository;
+  private final PsItemRepository psItemRepository;
   private final PsCartRepository psCartRepository;
 
 
   Mono<PsCart> addToCart(String cartId, String id) {
-    return this.psCartRepository.findById(cartId);
+    return this.psCartRepository.findById(cartId)
+            .defaultIfEmpty(new PsCart(cartId))
+            .flatMap(cart -> cart.getCartItems().stream()
+            .filter(cartItem -> cartItem.getItem().getId().equals(id))
+            .findAny()
+            .map(cartItem -> {
+              cartItem.increment();
+              return Mono.just(cart);
+            })
+            .orElseGet(() ->
+              this.psItemRepository.findById(id)
+                      .map(PsCartItem::new)
+                      .doOnNext(cartItem -> cart.getCartItems().add(cartItem))
+                      .map(cartItem -> cart)))
+            .flatMap(this.psCartRepository::save);
   }
 }
