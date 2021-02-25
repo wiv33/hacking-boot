bee3d7bbcf7c84bcfd1e9bcd37a89e1f6c7094d4
PSK <40161102+wiv33@users.noreply.github.com>
2021-02-25 23:14:15 +0900

    chap 02: add devtools

--- chap02-data-access/build.gradle.kts
+++ chap02-data-access/build.gradle.kts
@@ -4,5 +4,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.mongodb:mongodb-driver-sync")
 }
