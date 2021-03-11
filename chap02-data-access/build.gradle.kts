a83ea4a56a014cd68686224d64d20d1c1d0b2d02
PSK <wiv821253@gmail.com>
2021-03-11 11:02:35 +0900

    chap02 : test if not used BlockHound

--- chap02-data-access/build.gradle.kts
+++ chap02-data-access/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.mongodb:mongodb-driver-sync")
     implementation("io.projectreactor.tools:blockhound:1.0.3.RELEASE")
+    testImplementation("io.projectreactor.tools:blockhound-junit-platform:1.0.3.RELEASE")
 }
