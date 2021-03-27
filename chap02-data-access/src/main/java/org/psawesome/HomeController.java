27dcd881d492a65fa8226a4d0267d48080a6c4f9
PSK <wiv821253@gmail.com>
2021-03-27 16:25:23 +0900

    remove print

--- chap02-data-access/src/main/java/org/psawesome/HomeController.java
+++ chap02-data-access/src/main/java/org/psawesome/HomeController.java
@@ -33,8 +33,7 @@ public class HomeController {
   @GetMapping
   Mono<Rendering> home() {
     return Mono.just(Rendering.view("home.html")
-            .modelAttribute("items", this.psItemRepository.findAll()
-                    .doOnNext(System.out::println))
+            .modelAttribute("items", this.psItemRepository.findAll())
             .modelAttribute("cart", this.psCartRepository.findById("My Cart")
                     .defaultIfEmpty(new PsCart("My Cart"))).build());
   }
